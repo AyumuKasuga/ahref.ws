f50a06b19fedef978b63eb99c9beefb4be4b56dd
Ayumu Kasuga <bps@dzen.eu>
2012-10-28 15:50:05 +0400

    fix static files

--- ahrefws/urls.py
+++ ahrefws/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from ahrefws import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -16,5 +17,5 @@ urlpatterns = patterns('',
     url(r'^$', 'shorturl.views.main_page', name='main_page'),
     url(r'^add-url/', 'shorturl.views.add_url', name='add_url'),
     url(r'^u/(?P<url>.*)/', 'shorturl.views.follow_url', name='follow_url'),
-
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
 )
