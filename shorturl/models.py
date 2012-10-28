2b48ecdc21dfa65849d6b474662f7731721e1fe1
Ayumu Kasuga <bps@dzen.eu>
2012-10-28 21:22:18 +0400

    short_url_int field added

--- shorturl/models.py
+++ shorturl/models.py
@@ -6,7 +6,8 @@ from int_to_36 import int_to_36
 
 class ShortUrl(models.Model):
     source_url = models.URLField('source url', max_length=8192)
-    short_url = models.CharField('short url', max_length=128, blank=True, null=True)
+    short_url_int = models.PositiveIntegerField('short url int', default=0, blank=True, null=True, unique=True)
+    short_url = models.CharField('short url', max_length=128, blank=True, null=True, unique=True)
     created = models.DateTimeField('created', auto_now_add=True, blank=True, null=True, editable=False, default=0)
     clicks = models.PositiveIntegerField('clicks', default=0, blank=True, null=True)
 
@@ -14,11 +15,13 @@ class ShortUrl(models.Model):
         verbose_name = 'short url'
         verbose_name_plural = 'short urls'
 
+
     def __unicode__(self):
         return '%s (%s)' % (self.source_url[0:50], self.short_url)
 
     def save(self, *args, **kwargs):
-        last_object = ShortUrl.objects.all().aggregate(Max('pk'))
-        next_pk = last_object['pk__max'] or 0 + 1
-        self.short_url = int_to_36(next_pk)
+        last_object = ShortUrl.objects.all().aggregate(Max('short_url_int'))
+        next_short_url_int = last_object['short_url_int__max'] or 0 + 1
+        self.short_url_int = next_short_url_int
+        self.short_url = int_to_36(next_short_url_int)
         super(ShortUrl, self).save(*args, **kwargs)
