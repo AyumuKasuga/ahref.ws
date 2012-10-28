ae89bf290bf1073e60a53efe25e1777b3a4aa74b
Ayumu Kasuga <bps@dzen.eu>
2012-10-28 17:48:10 +0400

    remove global_host variable

--- ahrefws/urls.py
+++ ahrefws/urls.py
@@ -5,14 +5,6 @@ from ahrefws import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ahrefws.views.home', name='home'),
-    # url(r'^ahrefws/', include('ahrefws.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'shorturl.views.main_page', name='main_page'),
     url(r'^add-url/', 'shorturl.views.add_url', name='add_url'),
--- shorturl/views.py
+++ shorturl/views.py
@@ -4,18 +4,14 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils.simplejson import dumps
 from django.core.urlresolvers import reverse
-from django.contrib.sites.models import Site
-from ahrefws.settings import SITE_ID
 
 from shorturl.models import ShortUrl
 from shorturl.forms import AddUrlForm
 
-global_domain = Site.objects.get(pk=SITE_ID).domain
 
 def main_page(request):
     host_parts = request.get_host().split('.')
-    global_domain_parts = global_domain.split('.')
-    if len(host_parts) - len(global_domain_parts) == 1:
+    if len(host_parts) == 3:
         return follow_url(request, host_parts[0])
     return render_to_response('templates/index.html', {'form': AddUrlForm})
 
@@ -28,7 +24,7 @@ def add_url(request):
             if src_url.find('http://') != -1 and src_url.find('https://') != -1:
                 src_url = 'http://%s' % src_url
             parsed_url = urlparse.urlparse(src_url)
-            if parsed_url.netloc.find(global_domain) != -1:
+            if parsed_url.netloc.find(request.get_host()) != -1:
                 ret = {'status': 'fail', 'error': 'recursive url'}
             else:
                 try:
@@ -36,8 +32,8 @@ def add_url(request):
                 except ShortUrl.DoesNotExist:
                     url_obj = ShortUrl(source_url = src_url)
                     url_obj.save()
-                short_link = "http://%s.%s" % (url_obj.short_url, global_domain)
-                alt_short_link = 'http://%s%s' % (global_domain, reverse('shorturl.views.follow_url', args=(url_obj.short_url,)))
+                short_link = "http://%s.%s" % (url_obj.short_url, request.get_host())
+                alt_short_link = 'http://%s%s' % (request.get_host(), reverse('shorturl.views.follow_url', args=(url_obj.short_url,)))
                 ret = {'status': 'ok', 'short_url': short_link, 'alt_short_url': alt_short_link}
         else:
             ret = {'status': 'fail', 'error': 'not valid url'}
@@ -48,6 +44,7 @@ def follow_url(request, url):
     try:
         url_obj = ShortUrl.objects.get(short_url = url)
     except ShortUrl.DoesNotExist:
-        return HttpResponseRedirect('http://%s' % global_domain)
+        main_host = '.'.join(request.get_host().split('.')[1:])
+        return HttpResponseRedirect('http://%s' % main_host)
     else:
         return HttpResponseRedirect(url_obj.source_url)
