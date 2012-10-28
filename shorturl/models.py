1b00b57d38220adba7549226565d6eba9aa49358
Ayumu Kasuga <bps@dzen.eu>
2012-10-28 23:16:20 +0400

    bug fix

--- shorturl/models.py
+++ shorturl/models.py
@@ -20,7 +20,8 @@ class ShortUrl(models.Model):
         return '%s (%s)' % (self.source_url[0:50], self.short_url)
 
     def save(self, *args, **kwargs):
-        last_object = ShortUrl.objects.all().aggregate(Max('short_url_int'))
-        self.short_url_int = (last_object['short_url_int__max'] or 0) + 1
-        self.short_url = int_to_36(self.short_url_int)
+        if not self.pk:
+            last_object = ShortUrl.objects.all().aggregate(Max('short_url_int'))
+            self.short_url_int = (last_object['short_url_int__max'] or 0) + 1
+            self.short_url = int_to_36(self.short_url_int)
         super(ShortUrl, self).save(*args, **kwargs)
